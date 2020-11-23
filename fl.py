9aed034b5932cf94dd17346a2965d14e5b37f2f9
cab1986 <74911526+cab1986@users.noreply.github.com>
2020-11-23 22:03:12 +0300

    Add files via upload

--- fl.py
+++ fl.py
@@ -1,4 +1,5 @@
 from flask import *
+import os
 #from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
@@ -37,4 +38,5 @@ def form_sample():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
